17b79956bb420d78aae77e6dfb050bb90d0032f5
Alfonso Tienda <afoone@hotmail.com>
2021-05-12 13:46:20 +0200

    initial version

---
+++ 01-hello-world/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const App =  () => {
+
+
+  return (
+    <div className="app">
+      <h1 id="cabecera">Esto es una lista de cosas</h1>
+      <ul>
+        <li>cosa 1</li>
+        <li>cosa 2</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
