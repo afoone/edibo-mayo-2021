911756581fa629f8be333913740065157cb64078
Alfonso Tienda <afoone@hotmail.com>
2021-05-17 09:35:05 +0200

    comments

---
+++ 02-comments/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import CommentsList from './components/CommentsList'
+
+function App() {
+  return <div className="app">
+    <CommentsList/>
+  </div>;
+}
+
+export default App;
---
+++ 02-comments/src/components/CommentsList.jsx
@@ -0,0 +1,12 @@
+// Importamos la librería de React
+import React from 'react'
+
+// Creamos la función "componente"
+const CommentsList = () => {
+    return <div className="comments-list">
+        Lista de comentarios
+    </div>
+}
+
+// exportamos la función componente
+export default CommentsList
