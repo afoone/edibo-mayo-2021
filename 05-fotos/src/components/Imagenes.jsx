26b036853f88c3f7bee603bc4a4de4fbcb1c0ec5
Alfonso Tienda <afoone@hotmail.com>
2021-05-18 12:33:59 +0200

    consulta

--- 05-fotos/src/components/Imagenes.jsx
+++ 05-fotos/src/components/Imagenes.jsx
@@ -1,36 +1,63 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React, { Component } from "react";
+import axios from "axios";
 
 export class Imagenes extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imagenes: [],
+      consulta: ""
+    };
+  }
 
+  componentDidMount() {
+    axios
+      .get(
+        "https://api.unsplash.com/search/photos/?client_id=N4IxmojIOcsRn0NMD39tJwNIYJBhFDLUWJWO_1wpssU&query=beach"
+      )
+      .then((res) =>
+        this.setState({
+          imagenes: res.data.results,
+        })
+      );
+  }
 
-    constructor(props){
-        super(props)
-        this.state= {
-            imagenes: []
-        }
-    }
-
-    componentDidMount() {
-        axios.get('https://api.unsplash.com/search/photos/?client_id=N4IxmojIOcsRn0NMD39tJwNIYJBhFDLUWJWO_1wpssU&query=beach').then(
-            res => this.setState({
-                imagenes: res.data.results
-            })
-        )
-    }
-
-    render() {
-        return (
-            <div>
-                Imagenes
-                {
-                    this.state.imagenes.map(
-                        imagen => <img src={imagen.urls.thumb}/>
-                    )
-                }
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h3>Imágenes</h3>
+        <div>
+            <input type="text" value={this.state.consulta} onChange={
+                e => this.setState({
+                    consulta: e.target.value
+                })
+            }></input>
+            <button>Buscar</button>
+        </div>
+        <div
+          style={{
+            margin: "1rem",
+            display: "grid",
+            gridTemplateColumns: "auto auto auto auto auto auto ",
+            justifyContent: "space-between",
+            gap: "1rem",
+          }}
+        >
+          {this.state.imagenes.map((imagen) => {
+            console.log(imagen.width, imagen.height);
+            const apaisada = imagen.width > imagen.height;
+            console.log(apaisada);
+            const imgStyle = {
+              height: "10rem",
+              gridColumnEnd: apaisada ? "span 2" : null,
+              width: "100%",
+            };
+            return <img src={imagen.urls.thumb} style={imgStyle} />;
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Imagenes
+export default Imagenes;
