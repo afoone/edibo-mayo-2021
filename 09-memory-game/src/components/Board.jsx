ad1242a6294d89234fa98ca7198c306082f97bdf
Alfonso Tienda <afoone@hotmail.com>
2021-05-20 12:21:01 +0200

    cards

--- 09-memory-game/src/components/Board.jsx
+++ 09-memory-game/src/components/Board.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import './Board.css'
+import Card from './Card'
 
-const Board = () => {
+const Board = ({cols}) => {
+
+console.log(cols)
+
+    const templateGrid = "auto ".repeat(cols)
+    console.log(templateGrid)
+
+    const createCards = (n) => {
+        const response = []
+        for (let index = 0; index < n; index++) {
+            response.push(<Card/>)            
+        }
+        return response
+    }
+
     return (
-        <div>
-            Board
+        <div className="board" style={{gridTemplateColumns: templateGrid}}>
+            {
+                createCards(cols*cols)
+            }
         </div>
     )
 }
